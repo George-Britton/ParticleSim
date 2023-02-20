11b166d742fd66cc74a0de5621c96808a82180db
George Britton <george.britton97@hotmail.com>
2023-02-20 16:47:13 +0000

    Initial commit

---
+++ Source/PhysicsSandbox/CustomNodes.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CustomNodes.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHYSICSSANDBOX_API UCustomNodes : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+		static FVector GetDangerousReflectionVector(const FVector& Direction, const FVector& SurfaceNormal) {return (Direction - 2 * (Direction | SurfaceNormal) * SurfaceNormal);};
+	
+};
